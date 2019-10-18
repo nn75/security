8f28d5020ff77d2e379187e57b7aafecbcfc72a3
nn75 <ninan201718@gmail.com>
2019-10-17 21:11:31 -0400

    12 fix

--- hw3/12_question/build.sh
+++ hw3/12_question/build.sh
@@ -7,7 +7,7 @@ while read line; do
     var2=$(nmap  -sV -p 22 $line | perl -ne '/^Nmap scan report for (.*) \((.*)\)/ and print "$2\n"')
     var3=$(sudo scanssh -s ssh $line | perl -ne '/(SSH-.*)/ and print "$1\n"')
 
-    echo -e "$var1\t$var2\t$var3" >> out
+    echo -e "$var1:$var2:$var3" >> out.csv
 
 done < "$filename"
 
